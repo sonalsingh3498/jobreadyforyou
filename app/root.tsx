f9cda88c550ca7ccfa2991a23a6262a316c64c86
Sonal Singh <sonalsingh@Sonals-MacBook-Pro.local>
2025-07-11 00:54:30 +0530

    update

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,10 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "react-router";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
 import "./app.css";
 
-
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export default function App() {
-  return <Outlet />;
-}
-
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
